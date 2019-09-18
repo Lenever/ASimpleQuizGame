b6c38f7fd061f903d059250f45953e429b9807e2
Ikechukwu Onuorah <onuorah.ikechukwuvictor@gmail.com>
2019-09-18 09:27:38 +0100

    A little more front end work with add questions set up nicely

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-$(".dropdown").dropdown();
-$(document).ready(function() {});
+$(document).ready(function() {
+  $("#addquestion").submit(function(event) {
+    if ($(".question").val() !== "" && $(".answer").val() !== "") {
+      const question = $(".question").val();
+      const answer = $(".answer").val();
+      let newQuestions = {
+        question,
+        answer
+      };
+      $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/questions",
+        data: newQuestions
+      })
+        .done(function(newQuestions) {
+          alert("Question Saved: " + newQuestions);
+          const postData = JSON.stringify(newQuestions);
+          localStorage.setItem("post", postData);
+        })
+        .fail(function(err) {
+          alert("Error" + msg);
+        });
+    }
+    event.preventDefault();
+  });
+});
